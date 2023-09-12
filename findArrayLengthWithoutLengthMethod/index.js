cb409d55b97eec064d63f7228d9aa122f37d44d6
manojduraisamy7991 <manojduraisamy7991@gmail.com>
2023-09-12 21:29:35 +0530

    1st problem

---
+++ findArrayLengthWithoutLengthMethod/index.js
@@ -0,0 +1,12 @@
+let data = [1,2,4,5,6]
+let arrayLength = 0;
+
+function arrayCount ()
+{
+while(data[arrayLength]!== undefined){
+ arrayLength++
+}
+return arrayLength
+}
+console.log(arrayCount())
+
